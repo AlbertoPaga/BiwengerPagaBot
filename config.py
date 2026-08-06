97bc3e41af2a6fde4a6a40d78d37f21f6d40626e
AlbertoPaga <acarmonal97@gmail.com>
2026-08-06 12:39:18 +0200

    Usar variables de entorno Railway

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-BIWENGER_USER = os.getenv("BIWENGER_USER")
-BIWENGER_PASSWORD = os.getenv("BIWENGER_PASSWORD")
+BIWENGER_USER = os.getenv(
+    "BIWENGER_USER"
+)
+
+BIWENGER_PASSWORD = os.getenv(
+    "BIWENGER_PASSWORD"
+)
+
+TELEGRAM_TOKEN = os.getenv(
+    "TELEGRAM_TOKEN"
+)
